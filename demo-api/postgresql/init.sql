782f603f8e5d699ef9a0ce3fb7ba6543204fea07
Joffry Ferrater <joffry.ferrater@bisnode.com>
2019-12-20 14:59:22 +0100

    #2: postgresql dockerfile with employees table

---
+++ demo-api/postgresql/init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE employees
+(
+    id int NOT NULL,
+    username varchar(50) NOT NULL,
+    name varchar(250),
+    manager varchar(250),
+    salary int,
+    phoneNumber varchar(50)
+);
