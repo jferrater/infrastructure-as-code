17182ad7db604afcae13ea47304f168816eeb998
Joffry Ferrater <joffry.ferrater@bisnode.com>
2019-12-20 15:26:07 +0100

    #2: create test data

--- demo-api/postgresql/init.sql
+++ demo-api/postgresql/init.sql
@@ -7,3 +7,14 @@ CREATE TABLE employees
     salary int,
     phoneNumber varchar(50)
 );
+
+INSERT INTO employees (id, username, name, manager, salary, phoneNumber) VALUES(1, 'joffer', 'Joffry Ferrater', 'Jolly Jae Ompod', 66000, '1234567');
+INSERT INTO employees (id, username, name, manager, salary, phoneNumber) VALUES(2, 'alice', 'Alice Joe', 'Jolly Jae Ompod', 63000, '1234567');
+INSERT INTO employees (id, username, name, manager, salary, phoneNumber) VALUES(3, 'bob', 'Bob Joe', 'Jolly Jae Ompod', 50000, '1234567');
+INSERT INTO employees (id, username, name, manager, salary, phoneNumber) VALUES(4, 'mongkoy', 'Reihmon Estresmos', 'Jolly Jae Ompod', 73000, '1234567');
+INSERT INTO employees (id, username, name, manager, salary, phoneNumber) VALUES(5, 'peter', 'Peter Explorer', 'Wella Opalla', 35000, '1234567');
+INSERT INTO employees (id, username, name, manager, salary, phoneNumber) VALUES(6, 'john', 'John Lee', 'Wella Opalla', 66000, '1234567');
+INSERT INTO employees (id, username, name, manager, salary, phoneNumber) VALUES(7, 'janice', 'Janice Oscar', 'Wella Opalla', 33000, '1234567');
+INSERT INTO employees (id, username, name, manager, salary, phoneNumber) VALUES(8, 'dora', 'Dora Explorer', 'Jolly Jae Ompod', 49000, '1234567');
+INSERT INTO employees (id, username, name, manager, salary, phoneNumber) VALUES(9, 'leo', 'Leo Paslon', 'Jolly Jae Ompod', 43000, '1234567');
+INSERT INTO employees (id, username, name, manager, salary, phoneNumber) VALUES(10, 'muning', 'Muning Oe', 'Wella Opalla', 69000, '1234567');
