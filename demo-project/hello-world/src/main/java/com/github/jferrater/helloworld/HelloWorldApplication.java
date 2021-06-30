a23aa436a4ebb651a28ab53eb3539780180d2cc2
Joffry Ferrater <joffry.ferrater@trioptima.com>
2021-06-30 20:36:10 +0200

    - added demo project - re structure folders

---
+++ demo-project/hello-world/src/main/java/com/github/jferrater/helloworld/HelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.github.jferrater.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+}
