10159650bd4d59e410da4fdde90d9520beeadfab
Joffry Ferrater <joffry.ferrater@trioptima.com>
2021-07-07 19:22:43 +0200

    Created a hello world app for testing

---
+++ app/app.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello Jolly"
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8080)
